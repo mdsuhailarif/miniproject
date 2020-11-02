c9b8dbc48072f0ea7fbe69ddcb2d17504e1ee3d7
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-02 16:02:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
-# This function reads your DATABASE_URL config var and returns a connection
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["postgres://tgfotlqsesxkop:8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276@ec2-54-224-175-142.compute-1.amazonaws.com:5432/dcjrfn9ert59om"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1);
-}
+$host        = "host = ec2-54-224-175-142.compute-1.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = dcjrfn9ert59om";
+   $credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
 
-# Here we establish the connection. Yes, that's all.
-$db = pg_connect(pg_connection_string_from_database_url());
- if(!$db) {
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
       echo "Error : Unable to open database\n";
    } else {
       echo "Opened database successfully\n";
