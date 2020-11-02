41e96d7491f227bfe3027ea84f9932d4f1eaf9f8
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-02 21:13:03 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,7 +5,13 @@
    		$dbname      = "dbname = dcjrfn9ert59om";
    		$credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
 
-   		$db = pg_connect( "$host $port $dbname $credentials"  );
+   		$db = pg_connect( "$host $port $dbname $credentials");
+		  $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
 		 
 		$uname = $_POST['username'];
 		$email = $_POST['email'];
