e12aeb825358f1a69e9b1455eba2e675e13b6925
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-02 21:35:47 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,18 +1,30 @@
 <?php
-	 if($_POST){      
-       		$host        = "host = ec2-54-224-175-142.compute-1.amazonaws.com";
+
+		$host        = "host = ec2-54-224-175-142.compute-1.amazonaws.com";
    		$port        = "port = 5432";
    		$dbname      = "dbname = dcjrfn9ert59om";
    		$credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
 
    		$db = pg_connect( "$host $port $dbname $credentials");
-		  $db = pg_connect( "$host $port $dbname $credentials"  );
-   if(!$db) {
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   }
-		 
+
+	 if($_POST){      
+		$uname = $_POST['username'];
+		$email = $_POST['email'];
+		$passwrd = $_POST['password'];
+        
+        	$sql =<<<EOF
+      			INSERT INTO logind (USERNAME,EMAIL,PASSWORD)
+      			VALUES ('".$uname."','".$email."','".$passwrd."');
+		 EOF;
+
+   		$ret = pg_query($db, $sql);
+   		if(!$ret) {
+   		   echo "ERROR";
+   		} 
+		 else {
+   		   echo "Records created successfully\n";
+   		}
+ 
 		
    		pg_close($db);
 		 
