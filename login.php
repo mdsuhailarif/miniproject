55b0472ab2b3a903e8bbaaa811909edeee9227f4
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-02 14:28:26 +0530

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,14 @@ if( $_POST ){
 	
 	$id = $_POST[ 'username' ];
         
-    $db = new PDO('sqlite:userinfo.db');
+    	# This function reads your DATABASE_URL config var and returns a connection
+	function pg_connection_string_from_database_url() {
+  	extract(parse_url($_ENV["postgres://tgfotlqsesxkop:8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276@ec2-54-224-175-142.compute-1.amazonaws.com:5432/dcjrfn9ert59om"]));
+  	return "user=$user password=$pass host=$host dbname=" . substr($path, 1); 
+							}
+
+	# Here we establish the connection
+	$db = pg_connect(pg_connection_string_from_database_url());
         
         /* Create a prepared statement */
         $stmt = $db -> query("SELECT password from logind where username= :id or email = :id ");
@@ -20,7 +27,8 @@ if( $_POST ){
 		 if($res==$_POST['password']){
 		 header("location: dashboard.html ");}
 		 else{
-		 echo "login failed";}	 
+			 echo "Login failed! Try again.";
+		 }	 
 		 
         /* close connection */
         $db = null;
