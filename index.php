2975570ab6268c6355c3a616fc170dfecdba6f35
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-02 14:12:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php
-$host = "ec2-54-224-175-142.compute-1.amazonaws.com";
-$dbname = "dcjrfn9ert59om";
-$user = "tgfotlqsesxkop";
-$password = "8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
-$port = "5432";
-
-$dsn = "pgsql:host=$host;dbname=$dbname;user=$user;port=$port;password=$password";
-
-$db = new PDO($dsn);
-
-if($db){
-  echo "Connected <br />".$db;
-}else {
-  echo "Not connected";
+# This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["postgres://tgfotlqsesxkop:8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276@ec2-54-224-175-142.compute-1.amazonaws.com:5432/dcjrfn9ert59om"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 
+# Here we establish the connection. Yes, that's all.
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
 
+# Now let's use the connection for something silly just to prove it works:
+$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
 
+print "<pre>\n";
+if (!pg_num_rows($result)) {
+  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+} else {
+  print "Tables in your database:\n";
+  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+}
+print "\n";
 include_once("Homepage.html");
 
 ?>
