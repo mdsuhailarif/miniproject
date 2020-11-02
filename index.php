32ec9052c39d1dd0d74e83caf5b95e916ed8353f
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-02 14:45:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
+# This function reads your DATABASE_URL config var and returns a connection
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["postgres://tgfotlqsesxkop:8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276@ec2-54-224-175-142.compute-1.amazonaws.com:5432/dcjrfn9ert59om"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
 
+# Here we establish the connection. Yes, that's all.
+$db = pg_connect(pg_connection_string_from_database_url());
+
+$sql = ['CREATE TABLE IF NOT EXISTS logind (USERNAME VARCHAR(100) PRIMARY KEY, EMAIL  NOT NULL UNIQUE,PASSWORD TEXT);']
 
 include_once("Homepage.html");
 
