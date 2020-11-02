646764f5d0265b34e75f81645e61c805cdb1b7c4
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-02 14:22:43 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,13 @@
 <?php
 	 if($_POST){      
-        $db = new PDO('sqlite:userinfo.db');
+       		# This function reads your DATABASE_URL config var and returns a connection
+		function pg_connection_string_from_database_url() {
+ 		extract(parse_url($_ENV["postgres://tgfotlqsesxkop:8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276@ec2-54-224-175-142.compute-1.amazonaws.com:5432/dcjrfn9ert59om"]));
+  		return "user=$user password=$pass host=$host dbname=" . substr($path, 1); 
+		}
+
+		# Here we establish the connection
+		$db = pg_connect(pg_connection_string_from_database_url());
         
         /* inserting into database */
         $stmt = $db -> prepare("INSERT INTO logind (USERNAME, EMAIL, PASSWORD) VALUES (:USERNAME, :EMAIL, :PASSWORD)");
