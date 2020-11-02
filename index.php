d9c33efd820040b2918c9338062a19e0902b9c8d
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-02 14:19:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-# This function reads your DATABASE_URL config var and returns a connection
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["postgres://tgfotlqsesxkop:8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276@ec2-54-224-175-142.compute-1.amazonaws.com:5432/dcjrfn9ert59om"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); 
-}
 
-# Here we establish the connection
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 include_once("Homepage.html");
 
