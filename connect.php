7350007172f5f40f4f1f3566171f0b7926bb7bdc
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-03 09:52:59 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -9,12 +9,7 @@
 
 	
    $db = pg_connect( "$host $port $dbname $credentials"  );
-   if(!$db) {
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   }
-
+ 
         
         	$sql ="INSERT INTO logind VALUES ('$_POST[username]','$_POST[email]','$_POST[password]')";
 
