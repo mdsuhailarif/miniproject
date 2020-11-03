0e999e7b968ec8237bc86c915a7a886c77e35599
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-03 10:36:36 +0530

    Update login.php

--- login.php
+++ login.php
@@ -6,21 +6,14 @@
    $credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
- if(!$db) {
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   };
         
         /* Create a query statement */
-   $qry = pg_query($db, "SELECT password FROM logind WHERE username = '$_POST[username]' OR email = '$_POST[username]';);
-$result= pg_fetch_object($qry);
-if(strcmp($result,$_POST[password])) {
-    echo "Login Failed ! ";
-} else {
-    header(location: "daashborad.html");
-} 
+   $qry = pg_query($db, "SELECT * FROM logind WHERE username = '$_POST[username]' and password = '$_POST[password]';);
+    $login_check = pg_num_rows($qry);
+    if($login_check > 0){ {
+      echo "Login Failed ! "; }
+      else {
+      header(location: "daashborad.html");
+      } 
    
-        
-}	
 ?>
