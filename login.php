d487994dd31f5f511ccccd792c55bca710b23dd9
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-04 15:30:52 +0530

    Update login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ session_start();
    $db = pg_connect( "$host $port ${dbname $credentials"  );
 
    /* Create a query statement */
-   $qry = pg_query("SELECT password FROM logind WHERE email = "$_POST["email"]" and password = md5("$_POST["password"]")");
+   $qry = pg_query("SELECT password FROM logind WHERE email = "$_POST[email]" and password = md5("$_POST[password]")");
     $log = pg_fetch_array($qry);
        
 
