52b36250563b2c0eae73a5192ff8a131575b32ad
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-04 15:27:19 +0530

    Update login.php

--- login.php
+++ login.php
@@ -7,9 +7,9 @@ session_start();
    $credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
 
    $db = pg_connect( "$host $port ${dbname $credentials"  );
-        if($_POST){
+
    /* Create a query statement */
-   $qry = pg_query("SELECT password FROM logind WHERE email = '$_POST[email]' and password = md5('$_POST[password]')");
+   $qry = pg_query("SELECT password FROM logind WHERE email = '$_POST['email']' and password = md5('$_POST['password']')");
     $log = pg_fetch_array($qry);
        
 
@@ -18,7 +18,7 @@ session_start();
     else 
     {  echo "yes "; echo $log; echo $qry; include_once("dashboard.php"); } 
 
-$_SESSION['email']= $_POST[email];}
+$_SESSION["email"]= $_POST["email"];
 
 pg_close($db);
 ?>
