3c16e0b6dcb38f86b8c22a79b9eb94947fe8c796
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-04 16:22:35 +0530

    Update login.php

--- login.php
+++ login.php
@@ -6,13 +6,12 @@ session_start();
    $dbname      = "dbname = dcjrfn9ert59om";
    $credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
 
-   $db = pg_connect( "$host $port ${dbname $credentials"  );
+   $db = pg_connect( "$host $port $dbname $credentials"  );
 
    /* Create a query statement */
    $qry = pg_query("SELECT password FROM logind WHERE email = '$_POST[email]'");
-    $log = pg_fetch_array($qry);
-       
-
+    
+    $log = pg_fetch_assoc($qry);
     if($log > 0)
     { echo"no"; echo $log; include_once("signup.html"); }
     else 
