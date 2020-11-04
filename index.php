9959d1dbd128d0a9927e8e076e822d3f0fb81d75
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-04 14:22:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ PASS: ${{ secrets.DB_PASS }}
 $host        = "host = ec2-54-224-175-142.compute-1.amazonaws.com";
    $port        = "port = 5432";
    $dbname      = "dbname = dcjrfn9ert59om";
-   $credentials = "user = tgfotlqsesxkop password= $PASS ";
+   $credentials = "user = tgfotlqsesxkop password= getenv($PASS) ";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
 if($db)
