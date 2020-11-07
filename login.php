7678aff3aac8721db5d85695ee5e4b8061ab15db
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-07 17:40:14 +0530

    revert

--- login.php
+++ login.php
@@ -12,12 +12,15 @@ session_start();
 	
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
    $port        = "port = 5432";
    $dbname      = "dbname = dcjrfn9ert59om";
    $credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
 
 >>>>>>> parent of 7311972... patch
+=======
+>>>>>>> parent of a6e49a2... Update login.php
 =======
 >>>>>>> parent of a6e49a2... Update login.php
 =======
