923b6afd1273485e4e5f0245f639844d18a19b13
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-07 18:01:32 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+          $host        = "host = ec2-54-224-175-142.compute-1.amazonaws.com";
    		$port        = "port = 5432";
    		$dbname      = "dbname = dcjrfn9ert59om";
    		$credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
