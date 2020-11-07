5022375ea8bed5be261722ae39d4c625017276e4
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-07 17:30:07 +0530

    fiexd

--- login.php
+++ login.php
@@ -1,7 +1,15 @@
 <?php
 session_start();
 
-   include_once("connect.php");
+   $host        = "host = ec2-54-224-175-142.compute-1.amazonaws.com";
+   		$port        = "port = 5432";
+   		$dbname      = "dbname = dcjrfn9ert59om";
+   		$credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
+
+   		$db = pg_connect( "$host $port $dbname $credentials");
+
+	
+   $db = pg_connect( "$host $port $dbname $credentials"  );
 
    /* Create a query statement */
    $qry = pg_query($db,"SELECT * FROM logind WHERE email = '$_POST[email]' and password = md5('$_POST[password]') ");
