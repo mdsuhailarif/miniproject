df92fcb3d89541f823048de240ce5dce2926e872
Md Suhail Arif <suhailarif14@gmail.com>
2020-11-07 17:57:01 +0530

    update

--- login.php
+++ login.php
@@ -1,37 +1,12 @@
 <?php
 session_start();
 
-   $host        = "host = ec2-54-224-175-142.compute-1.amazonaws.com";
-<<<<<<< HEAD
-<<<<<<< HEAD
+		$host        = "host = ec2-54-224-175-142.compute-1.amazonaws.com";
    		$port        = "port = 5432";
    		$dbname      = "dbname = dcjrfn9ert59om";
    		$credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
 
    		$db = pg_connect( "$host $port $dbname $credentials");
-
-	
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of 7311972... patch
-   $port        = "port = 5432";
-   $dbname      = "dbname = dcjrfn9ert59om";
-   $credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
-
-<<<<<<< HEAD
->>>>>>> parent of 7311972... patch
-=======
->>>>>>> parent of a6e49a2... Update login.php
-=======
->>>>>>> parent of a6e49a2... Update login.php
-=======
->>>>>>> parent of a6e49a2... Update login.php
-=======
->>>>>>> parent of 7311972... patch
-   $db = pg_connect( "$host $port $dbname $credentials"  );
 
    /* Create a query statement */
    $qry = pg_query($db,"SELECT * FROM logind WHERE email = '$_POST[email]' and password = md5('$_POST[password]') ");
