70453a36a4f0cd731c9807b50d0d88802526c837
Md Suhail Arif <suhailarif14@gmail.com>
2021-01-27 16:18:54 +0530

    Update booking.php

--- booking.php
+++ booking.php
@@ -1,3 +1,30 @@
+<?php
+session_start();
+$host        = "host = ec2-54-224-175-142.compute-1.amazonaws.com";
+$port        = "port = 5432";
+$dbname      = "dbname = dcjrfn9ert59om";
+$credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
+$db = pg_connect( "$host $port $dbname $credentials");
+
+$sql =<<<EOF
+   CREATE TABLE IF NOT EXISTS booking
+   (USERNAME CHAR(100) ,
+    NO TEXT  NOT NULL UNIQUE Primary Key,
+    FROM TEXT, TO TEXT,BY TEXT,D DATE); 
+EOF;
+
+$ret = pg_query($sql);
+   		if(!$ret) {
+                echo "Success";
+   		} 
+		 else {
+           echo "Error";
+   		}
+pg_close($db);
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 
