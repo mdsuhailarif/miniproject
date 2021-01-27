15738f8e696f9062fd313d6b7192b32ada387187
Md Suhail Arif <suhailarif14@gmail.com>
2021-01-27 15:36:33 +0530

    Update login.php

--- login.php
+++ login.php
@@ -18,3 +18,25 @@ include_once("signup.html"); }
 
 pg_close($db);
 ?>
+
+<?php
+$host        = "host = ec2-54-205-187-125.compute-1.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = dbd2u0mqavsijh";
+   $credentials = "user = hdyundzmldxrzc password= c4e7157567aff1598b6b75c9373abd02f8bbdb2e10fa7abefbd07925e5d267bb ";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+
+$sql =<<<EOF
+   CREATE TABLE IF NOT EXISTS booking
+   (USERNAME CHAR(100) ,
+    NO TEXT  NOT NULL UNIQUE Primary Key,
+    FROM TEXT, TO TEXT,BY TEXT,D DATE); 
+EOF;
+
+ret=pg_query($sql);
+
+   pg_close($db);
+
+
+?>
