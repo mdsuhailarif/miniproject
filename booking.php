12fe998616a705ea1c58b67f70f4c09c13e0f891
Md Suhail Arif <suhailarif14@gmail.com>
2021-01-27 21:14:24 +0530

    Update booking.php

--- booking.php
+++ booking.php
@@ -186,7 +186,13 @@ EOF;
       </thead>
       <tbody>
 	  <?php
-include_once("book.php");
+$host        = "host = ec2-54-224-175-142.compute-1.amazonaws.com";
+   		$port        = "port = 5432";
+   		$dbname      = "dbname = dcjrfn9ert59om";
+   		$credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
+
+   		$db = pg_connect( "$host $port $dbname $credentials");
+		
 
    /* Create a query statement */	
 $qry = pg_query($db,"SELECT username,daa,frm,too FROM logind ");
