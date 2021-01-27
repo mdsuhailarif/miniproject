179696beda26bdd042c27b282d4e85f7cd51e287
Md Suhail Arif <suhailarif14@gmail.com>
2021-01-27 16:16:45 +0530

    Update login.php

--- login.php
+++ login.php
@@ -6,22 +6,6 @@ $dbname      = "dbname = dcjrfn9ert59om";
 $credentials = "user = tgfotlqsesxkop password=8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276";
 $db = pg_connect( "$host $port $dbname $credentials");
 
-$sql =<<<EOF
-   CREATE TABLE IF NOT EXISTS booking
-   (USERNAME CHAR(100) ,
-    NO TEXT  NOT NULL UNIQUE Primary Key,
-    FROM TEXT, TO TEXT,BY TEXT,D DATE); 
-EOF;
-
-$ret = pg_query($sql);
-   		if(!$ret) {
-   		
-                echo "Success";
-   		} 
-		 else {
-           echo "Error";
-   		}
-
    /* Create a query statement */	
 $qry = pg_query($db,"SELECT * FROM logind WHERE email = '$_POST[email]' and password = md5('$_POST[password]') ");
 $row= pg_fetch_row($qry);
