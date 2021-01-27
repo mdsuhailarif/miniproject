a58be0b91e4745ec39e198d4e91956c8646bd6b7
Md Suhail Arif <suhailarif14@gmail.com>
2021-01-27 15:44:20 +0530

    Update login.php

--- login.php
+++ login.php
@@ -20,10 +20,10 @@ pg_close($db);
 ?>
 
 <?php
-$host        = "host = ec2-54-205-187-125.compute-1.amazonaws.com";
+$host        = "host = ec2-54-224-175-142.compute-1.amazonaws.com";
    $port        = "port = 5432";
-   $dbname      = "dbname = dbd2u0mqavsijh";
-   $credentials = "user = hdyundzmldxrzc password= c4e7157567aff1598b6b75c9373abd02f8bbdb2e10fa7abefbd07925e5d267bb ";
+   $dbname      = "dbname = dcjrfn9ert59om";
+   $credentials = "user = tgfotlqsesxkop password= 8347981a2330af6e15c433302db79ab3d9ba7f7f32cd25dbabd0dff7e78f6276 ";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
 
@@ -34,9 +34,8 @@ $sql =<<<EOF
     FROM TEXT, TO TEXT,BY TEXT,D DATE); 
 EOF;
 
-ret=pg_query($sql);
+ret = pg_query($sql);
 
    pg_close($db);
-
 
 ?>
