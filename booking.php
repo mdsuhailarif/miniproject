ae1bb160effd29ed7ac0b9486475e5a1bcd00f7b
Md Suhail Arif <suhailarif14@gmail.com>
2021-01-27 23:27:08 +0530

    Update booking.php

--- booking.php
+++ booking.php
@@ -36,12 +36,6 @@
 <div class="header">
 		<nav class="navbar navbar-default">
 					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" style= "opacity:0.3;">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
 						<h1><a href="dashboard.html"><img src="images/logo.png" alt="logo" height="100"></a></h1>
 					</div>
 
