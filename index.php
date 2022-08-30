910a9445477ae420935d7837539e36b71d0e8d76
Md Suhail Arif <suhailarif14@gmail.com>
2022-08-30 12:30:42 +0530

    revert

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-   include("connect.php");
    
+   $host        = "host = ec2-52-73-213-161.compute-1.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = d6j1bk6en4rbf0";
+   $credentials = "user = ncxgxfhqlrypnf password= ba2d16e7b5efb523d226e748c876ba6c3a5d55b9baa8a5531d14e8c275229c80 ";
+
    $db = pg_connect( "$host $port $dbname $credentials"  );
 
 $sql =<<<EOF
