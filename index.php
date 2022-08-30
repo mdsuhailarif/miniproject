830c43da3bc68b4d21029c24e3aa6bb9150b3748
Md Suhail Arif <suhailarif14@gmail.com>
2022-08-30 12:59:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
 
-$sql ="CREATE TABLE logind (USERNAME CHAR(100) , EMAIL TEXT  NOT NULL UNIQUE Primary Key, PASSWORD TEXT, N TEXT, FRM TEXT, TOO TEXT, BYE TEXT, DA DATE,agency TEXT)";
+$sql ="CREATE TABLE logind (USERNAME CHAR(100) , EMAIL TEXT  NOT NULL UNIQUE Primary Key, PASSWORD TEXT, N TEXT, FRM TEXT, TOO TEXT, BYE TEXT, DA DATE,agency TEXT);";
 pg_query($db,$sql);
 
 
