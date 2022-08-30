92dc65477290a98068254403086da803f78f2144
Md Suhail Arif <suhailarif14@gmail.com>
2022-08-30 13:13:53 +0530

    Update book.php

--- book.php
+++ book.php
@@ -6,9 +6,7 @@
    		$credentials = "user = ncxgxfhqlrypnf password=ba2d16e7b5efb523d226e748c876ba6c3a5d55b9baa8a5531d14e8c275229c80";
 
    		$db = pg_connect( "$host $port $dbname $credentials");
-		
-		$email=$_POST[email];
-		
+				
         $sql ="UPDATE logind SET n='$_POST[no]',frm='$_POST[frm]',too='$_POST[too]',bye='$_POST[bye]',da='$_POST[da]' WHERE email='$_POST[email]';";
 		$ret = pg_query($db,$sql);
    		if(!$ret) 
